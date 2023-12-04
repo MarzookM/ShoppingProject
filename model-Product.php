cdb56f7ec605e42efff58f7c5af8b4766526954b
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-04 00:35:50 -0600

    Update model-Product.php

--- model-Product.php
+++ model-Product.php
@@ -12,22 +12,6 @@ function selectProduct(){
         throw $e;
     }
 }
-function getImageDataFromBlob($blobData)
-{
-    // Assuming you have a database connection object named $con
-    // Replace 'your_table' and 'your_blob_column' with your actual table and column names
-    $sql = "SELECT ProductImage FROM your_table WHERE your_blob_column = ?";
-    $stmt = $con->prepare($sql);
-    $stmt->bind_param("s", $blobData);
-    $stmt->execute();
-    $result = $stmt->get_result();
 
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        return $row['ProductImage'];
-    } else {
-        return null;
-    }
-}
 
 ?>
