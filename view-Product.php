568460174d5cf261c700190b527ec9b14133b1b8
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-04 03:43:13 -0600

    Update view-Product.php

--- view-Product.php
+++ view-Product.php
@@ -1,40 +1,32 @@
 <h1>Products</h1>
-<div class="table-responsive">
-    <table class="table">
-        <tbody>
-            <?php
-            // Assuming $Product is your fetched result from the control page
-            while ($Products = $Product->fetch_assoc()) {
-            ?>
-                <tr>
-                    <td>
-                        <div class="card">
-                            <?php echo '<img src="data:image;base64,' . base64_encode($Products['ProductImage']) . '" alt="Product Image" style="width:100px; height:100px;">'; ?>
-                            <div class="card-body">
-                                <h5 class="card-title"><?php echo $Products['ProductName']; ?></h5>
-                                <p class="card-text">Price: $<?php echo $Products['ProductPrice']; ?></p>
-                                <form action="addToCart.php" method="POST">
-                                    <input type="hidden" name="productId" value="<?php echo $Products['ProductID']; ?>">
-                                    <label for="quantity">Quantity:</label>
-                                    <select class="form-control" name="quantity">
-                                        <?php
-                                      
-                                        // Display dropdown options from 0 to 10
-                                        for ($i = 0; $i <= 10; $i++) {
-                                            echo "<option value=\"$i\">$i</option>";
-                                        }
-                                        ?>
-                                    </select>
-                                    <button type="submit" class="btn btn-primary">Add to Cart</button>
-                                </form>
-                            </div>
-                        </div>
-                    </td>
-                </tr>
-            <?php
-            }
-            ?>
-        </tbody>
-    </table>
+<div class="row">
+    <?php
+    // Assuming $Product is your fetched result from the control page
+    while ($Products = $Product->fetch_assoc()) {
+    ?>
+        <div class="col-md-4">
+            <div class="card">
+                <?php echo '<img src="data:image;base64,' . base64_encode($Products['ProductImage']) . '" alt="Product Image" class="card-img-top">'; ?>
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $Products['ProductName']; ?></h5>
+                    <p class="card-text">Price: $<?php echo $Products['ProductPrice']; ?></p>
+                    <form action="addToCart.php" method="POST">
+                        <input type="hidden" name="productId" value="<?php echo $Products['ProductID']; ?>">
+                        <label for="quantity">Quantity:</label>
+                        <select class="form-control" name="quantity">
+                            <?php
+                            // Display dropdown options from 0 to 10
+                            for ($i = 0; $i <= 10; $i++) {
+                                echo "<option value=\"$i\">$i</option>";
+                            }
+                            ?>
+                        </select>
+                        <button type="submit" class="btn btn-primary">Add to Cart</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    <?php
+    }
+    ?>
 </div>
-
