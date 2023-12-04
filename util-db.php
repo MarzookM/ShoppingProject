e9f23823cc2a2ec8caf0d0e840ef1002ed20af64
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-03 23:45:11 -0600

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli('159.89.47.44', 'misgoldo_UserM' ,'Poochiecat12@','misgoldo_shoppingproject');
+    $conn = new mysqli('159.89.47.44', 'misgoldo_UserM' ,'Poochiecat12@','misgoldo_shoppingProject');
     
     // Check connection
     if ($conn->connect_error) {
