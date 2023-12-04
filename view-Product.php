36ea2c667cf570b3db2f39d8d786fc426d68fd65
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-04 03:03:42 -0600

    Update view-Product.php

--- view-Product.php
+++ view-Product.php
@@ -13,9 +13,19 @@
                             <div class="card-body">
                                 <h5 class="card-title"><?php echo $Products['ProductName']; ?></h5>
                                 <p class="card-text">Price: $<?php echo $Products['ProductPrice']; ?></p>
-                                <form action="" method="POST">
+                                <form action="addToCart.php" method="POST">
                                     <input type="hidden" name="productId" value="<?php echo $Products['ProductID']; ?>">
-                                    <input type="hidden" name="quantity" value="1"> <!-- Assuming default quantity is 1 -->
+                                    <label for="quantity">Quantity:</label>
+                                    <select class="form-control" name="quantity">
+                                        <?php
+                                        // Display dropdown options based on the current quantity
+                                        for ($i = 1; $i <= $Products['ProductQuantity']; $i++) {
+                                        // Display dropdown options from 0 to 10
+                                        for ($i = 0; $i <= 10; $i++) {
+                                            echo "<option value=\"$i\">$i</option>";
+                                        }
+                                        ?>
+                                    </select>
                                     <button type="submit" class="btn btn-primary">Add to Cart</button>
                                 </form>
                             </div>
@@ -28,34 +38,3 @@
         </tbody>
     </table>
 </div>
-
-<?php
-// Include addToCart.php logic here
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['productId'], $_POST['quantity'])) {
-    include("addToCart.php");
-
-    $productId = $_POST['productId'];
-    $quantityToAdd = $_POST['quantity'];
-
-    // Fetch the current quantity from the database
-    $currentQuantity = getCurrentQuantity(get_db_connection(), $productId);
-
-    if ($currentQuantity !== false) {
-        // Calculate the new quantity
-        $newQuantity = $currentQuantity + $quantityToAdd;
-
-        // Update the quantity in the database
-        updateProductQuantity(get_db_connection(), $productId, $newQuantity);
-
-        // Display success message
-        echo '<div class="alert alert-success" role="alert">
-                Added to Cart successfully!
-            </div>';
-    } else {
-        // Display error message
-        echo '<div class="alert alert-danger" role="alert">
-                Product not found or an error occurred.
-            </div>';
-    }
-}
-?>
