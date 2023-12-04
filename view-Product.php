114367f324ff9e00e5f38384d1dbc7a904facf04
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-03 23:42:02 -0600

    Create view-Product.php

---
+++ view-Product.php
@@ -0,0 +1,34 @@
+<h1>Product</h1>
+<div class = "table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Price</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+<?php                 //from control page
+while($Products = $Product ->fetch_assoc())
+  {
+?>
+<tr> 
+  <td><?php echo $Products['ProductID']; ?></td>
+  <td><?php echo $Products['ProductName']; ?></td>  
+  <td><?php echo $Products['ProductPrice']; ?></td>
+  <td><?php echo $Products['ProductQuantity']; ?></td>  
+  <td><?php echo $Products['ProductCart']; ?></td>
+ 
+</tr>
+<?php
+    
+  }
+?>
+      
+    </tbody>
+
+
+      
+    </thead>
