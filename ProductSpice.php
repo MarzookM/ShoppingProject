80416e5c3dcd6060a1c3200b3d2aab0c46f275db
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-05 05:55:25 -0600

    Delete ProductSpice.php

--- ProductSpice.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("util-db.php");
-require_once("model-ProductSpice.php");
-$pageTitle = "Customer";
-include "view-header.php";
-$ProductSpice = selectProductSpice();
-include "view-ProductSpice.php";
-include "view-footer.php";
-?>
