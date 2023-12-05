5ff54e76e23712a5fa1676626300dd3726ad6744
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-05 03:38:09 -0600

    Create cart.php

---
+++ cart.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+
+$pageTitle = "Cart";
+include "view-header.php";
+include "view-Product.php";
+include "view-footer.php";
+include("model-cart.php");
+include("view-cart.php");
+?>
