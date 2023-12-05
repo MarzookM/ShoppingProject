a450ac9f5592dcd84b9412f1b4a8ad5c939659d7
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-05 03:41:16 -0600

    Update cart.php

--- cart.php
+++ cart.php
@@ -3,7 +3,8 @@ require_once("util-db.php");
 
 $pageTitle = "Cart";
 include "view-header.php";
-include "view-footer.php";
-include("model-cart.php");
 include("view-cart.php");
+include "view-footer.php";
+
+
 ?>
