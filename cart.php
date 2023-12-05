7153846baabef80ad1f4a632e6291c162eeee5bb
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-05 05:10:20 -0600

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,9 +1,32 @@
 <?php
 require_once("util-db.php");
 require_once("model-cart.php");
-$pageTitle = "Customer";
+$pageTitle = "Cart";
 include "view-header.php";
-$CartProduct = selectCartProducts();
-include "view-cart.php";
+
+if (isset($_POST['actionType'])) {
+    switch ($_POST['actionType']) {
+        case "Add":
+            // Add your logic for adding to cart here
+            break;
+        case "Edit":
+            if (updateCartProductQuantity($_POST['quantity'], $_POST['productId'])) {
+                echo '<div class="alert alert-success" role="alert">Product quantity updated.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error updating product quantity.</div>';
+            }
+            break;
+        case "Delete":
+            if (deleteCartProduct($_POST['productId'])) {
+                echo '<div class="alert alert-success" role="alert">Product removed from cart.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error removing product from cart.</div>';
+            }
+            break;
+    }
+}
+
+$cartProducts = getCartProducts();
+include "view-cart.php"; // Assuming this is where you display the cart
 include "view-footer.php";
 ?>
