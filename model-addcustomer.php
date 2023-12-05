c7ef00f2192c0dd385a4e1ca021db8372b7f3682
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-05 06:14:09 -0600

    Update model-addcustomer.php

--- model-addcustomer.php
+++ model-addcustomer.php
@@ -5,6 +5,11 @@ function insertCustomer($customerName, $customerID, $customerPhoneNumber) {
         $stmt = $conn->prepare("INSERT INTO Customer (CustomerName, CustomerID, CustomerPhoneNumber) VALUES (?, ?, ?)");
         $stmt->bind_param("sss", $customerName, $customerID, $customerPhoneNumber);
         $success = $stmt->execute();
+
+        if (!$success) {
+            throw new Exception($stmt->error);
+        }
+
         $conn->close();
         return $success;
     } catch (Exception $e) {
@@ -12,4 +17,5 @@ function insertCustomer($customerName, $customerID, $customerPhoneNumber) {
         throw $e;
     }
 }
+
 ?>
