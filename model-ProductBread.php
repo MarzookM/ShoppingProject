88744ba6bf2834f92e4ed7156ed65f2d6f98719b
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-04 18:48:52 -0600

    Update model-ProductBread.php

--- model-ProductBread.php
+++ model-ProductBread.php
@@ -2,7 +2,7 @@
 function selectProductBread(){
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT `ProductID`, `ProductName`, `ProductPrice`, `ProductQuantity`, `ProductCart`, `ProductImage` FROM `Product`  "); 
+        $stmt = $conn->prepare("SELECT `ProductBreadID`, `ProductBreadName`, `ProductBreadPrice`, `ProductBreadQuantity`, `ProductBreadCart`, `ProductBreadImage` FROM `ProductBread`  "); 
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
