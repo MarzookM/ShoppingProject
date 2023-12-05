9b6ee62d0cdd2a8f25a7ac93273a778423e69d8a
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-05 05:55:14 -0600

    Delete ProductMeat.php

--- ProductMeat.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("util-db.php");
-require_once("model-ProductMeat.php");
-$pageTitle = "Meats";
-include "view-header.php";
-$ProductMeat = selectProductMeat();
-include "view-ProductMeat.php";
-include "view-footer.php";
-?>
