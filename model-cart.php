1ee706881d98d9ba039d93f7840b32dea3ccc31e
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-05 04:55:12 -0600

    Update model-cart.php

--- model-cart.php
+++ model-cart.php
@@ -2,7 +2,18 @@
 function selectCartProducts(){
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT Product.ProductName, Product.ProductQuantity FROM Store JOIN Product ON Store.ProductID = Product.ProductID WHERE Product.ProductCart = 'y' AND Product.ProductQuantity > 0"); 
+        $stmt = $conn->prepare("
+            SELECT 
+                Product.ProductName, 
+                Product.ProductQuantity, 
+                Product.ProductPrice, 
+                (Product.ProductQuantity * Product.ProductPrice) AS FinalPrice 
+            FROM 
+                Store 
+            JOIN 
+                Product ON Store.ProductID = Product.ProductID 
+            WHERE 
+                Product.ProductCart = 'y' AND Product.ProductQuantity > 0"); 
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
