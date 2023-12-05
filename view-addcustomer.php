b4a4328d64c3548eb6851b394451f634315c0708
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-05 06:33:24 -0600

    Update view-addcustomer.php

--- view-addcustomer.php
+++ view-addcustomer.php
@@ -1,17 +1,24 @@
+<?php
+// Include model-addcustomer.php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Handle form submission
+    $customerName = $_POST["customerName"];
+    $customerID = $_POST["customerID"];
+    $customerPhoneNumber = $_POST["customerPhoneNumber"];
+
+    $success = insertCustomer($customerName, $customerID, $customerPhoneNumber);
+
+    if ($success) {
+        // Redirect to Customers.php
+        header("Location: Customers.php");
+        exit();
+    }
+}
+?>
 <!-- Your HTML form for adding a customer -->
 <!-- Make sure to adjust the form structure based on your requirements -->
-<form method="post" action="model-addcustomer.php">
-    <div class="mb-3">
-        <label for="customerName" class="form-label">Customer Name</label>
-        <input type="text" class="form-control" id="customerName" name="customerName" required>
-    </div>
-    <div class="mb-3">
-        <label for="customerID" class="form-label">Customer ID</label>
-        <input type="text" class="form-control" id="customerID" name="customerID" required>
-    </div>
-    <div class="mb-3">
-        <label for="customerPhoneNumber" class="form-label">Customer Phone Number</label>
-        <input type="text" class="form-control" id="customerPhoneNumber" name="customerPhoneNumber" required>
-    </div>
-    <button type="submit" class="btn btn-primary" name="addCustomer">Add Customer</button>
+<form method="post" action="">
+    <!-- Your form fields here -->
+    <button type="submit" class="btn btn-primary">Add Customer</button>
 </form>
