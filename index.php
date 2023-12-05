81fb908f2161f18cbd7898036a4a62d5aa7d04fb
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-05 07:40:20 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?> 
-<object data="ou.mp4" width="400" height="300"></object>
+<object data="WelcomeVid.mp4" width="400" height="300"></object>
 <?php
 include "view-footer.php";
 ?> 
