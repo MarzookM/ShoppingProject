05051bb84812a01af9b0c47fe6beb93432ff6263
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-04 20:15:23 -0600

    Create model-ProductDrink.php

---
+++ model-ProductDrink.php
@@ -0,0 +1,17 @@
+<?php
+function selectProductDrink(){
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT `ProductDrinkID`, `ProductDrinkName`, `ProductDrinkPrice`, `ProductDrinkQuantity`, `ProductDrinkCart`, `ProductDrinkImage` FROM `ProductDrink`  "); 
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+?>
