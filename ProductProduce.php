f085e1143a6a3059e557fc7f7133b643344d25a6
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-04 21:39:55 -0600

    Update ProductProduce.php

--- ProductProduce.php
+++ ProductProduce.php
@@ -1,1 +1,9 @@
-
+<?php
+require_once("util-db.php");
+require_once("model-ProductProduce.php");
+$pageTitle = "Drinks";
+include "view-header.php";
+$ProductProduce = selectProductProduce();
+include "view-ProductProduce.php";
+include "view-footer.php";
+?>
