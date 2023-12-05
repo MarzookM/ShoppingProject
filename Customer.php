1805259d8a7398bf46cfddc69224aa3895a5dfac
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-05 06:40:08 -0600

    Create Customer.php

---
+++ Customer.php
@@ -0,0 +1,37 @@
+<?php
+require_once("util-db.php");
+require_once("model-Customer.php");
+$pageTitle = "Customer";
+include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if(insertCustomer($_POST['cNumber'], $_POST['cDesc'])){
+        echo '<div class="alert alert-success" role="alert">Customer Added.</div>';
+      }else{
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Edit":
+      if(updateCustomer($_POST['cDesc'], $_POST['cid'])){
+        echo '<div class="alert alert-success" role="alert">Customer edited.</div>';
+      }else{
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Delete":
+     if(deleteCustomer($_POST['cid'])){
+        echo '<div class="alert alert-success" role="alert">Customer Deleted.</div>';
+      }else{
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    
+  }
+}
+
+$Customer = selectCustomer();
+include "view-Customer.php";
+include "view-footer.php";
+?>
