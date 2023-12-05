f77825997d30077ac640c844bf765b52c9ccf6d5
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-05 04:52:13 -0600

    Update view-cart.php

--- view-cart.php
+++ view-cart.php
@@ -5,15 +5,18 @@
             <tr>
                 <th>Name</th>
                 <th>Quantity</th>
+                <th>Price</th> <!-- Added Price column -->
             </tr>
         </thead>
         <tbody>
             <?php
             while ($row = $CartProduct->fetch_assoc()) {
+                $finalPrice = $row['ProductPrice'] * $row['ProductQuantity'];
             ?>
                 <tr>
                     <td><?php echo $row['ProductName']; ?></td>
                     <td><?php echo $row['ProductQuantity']; ?></td>
+                    <td><?php echo $finalPrice; ?></td>
                 </tr>
             <?php
             }
