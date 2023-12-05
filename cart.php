cb4ada253c54916277d681cb194a2944bd687661
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-05 04:33:14 -0600

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,15 +1,9 @@
 <?php
 require_once("util-db.php");
-require_once("model-cart.php");
-$pageTitle = "Shopping Cart";
-include "header.php";
-
-$productBreadData = getCartData("ProductBread", "ProductBreadCart", "ProductBreadQuantity");
-$productProduceData = getCartData("ProductProduce", "ProductProduceCart", "ProductProduceQuantity");
-$productDrinkData = getCartData("ProductDrink", "ProductDrinkCart", "ProductDrinkQuantity");
-$productMeatData = getCartData("ProductMeat", "ProductMeatCart", "ProductMeatQuantity");
-$productSpiceData = getCartData("ProductSpice", "ProductSpiceCart", "ProductSpiceQuantity");
-
-include "view-cart.php";
-include "footer.php";
-?>
+require_once("model-cart.php"); // going to create 
+$pageTitle = "Customer";
+include "view-header.php";
+$Cart = selectCart();
+include "view-cart.php"; //going to create
+include "view-footer.php";
+?> 
