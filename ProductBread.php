360f4468ad5849fe2cc6c6f6b84aafea0bb132ec
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-05 05:54:48 -0600

    Delete ProductBread.php

--- ProductBread.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("util-db.php");
-require_once("model-ProductBread.php");
-$pageTitle = "Grains";
-include "view-header.php";
-$ProductBread = selectProductBread();
-include "view-ProductBread.php";
-include "view-footer.php";
-?>
