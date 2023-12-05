0db9bfc0e82b522bb9978574418f3f91ab46c692
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-05 03:08:10 -0600

    Update Cart.php

--- Cart.php
+++ Cart.php
@@ -1,37 +1,13 @@
 <?php
-require_once("util-db.php");
-require_once("model-Cart.php");
-$pageTitle = "Cart";
-include "view-header.php";
+include("model-cart.php");
 
-if (isset($_POST['actionType'])) {
-  switch ($_POST['actionType']) {
-    case "Add":
-      if(insertCart($_POST['cNumber'], $_POST['cDesc'])){
-        echo '<div class="alert alert-success" role="alert">Cart Added.</div>';
-      }else{
-        echo '<div class="alert alert-danger" role="alert">Error.</div>';
-      }
-      break;
-    case "Edit":
-      if(updateCart($_POST['cNumber'], $_POST['cDesc'],$_POST['cid'])){
-        echo '<div class="alert alert-success" role="alert">Cart edited.</div>';
-      }else{
-        echo '<div class="alert alert-danger" role="alert">Error.</div>';
-      }
-      break;
-    case "Delete":
-     if(deleteCart($_POST['cid'])){
-        echo '<div class="alert alert-success" role="alert">Course Deleted.</div>';
-      }else{
-        echo '<div class="alert alert-danger" role="alert">Error.</div>';
-      }
-      break;
-    
-  }
-}
+// Fetch cart items for each product type
+$breadCart = getCartItems("ProductBread");
+$spiceCart = getCartItems("ProductSpice");
+$drinkCart = getCartItems("ProductDrink");
+$meatCart = getCartItems("ProductMeat");
+$produceCart = getCartItems("ProductProduce");
 
-$Cart = selectCart();
-include "view-Cart.php";
-include "view-footer.php";
+// Include the view to display the cart
+include("view-cart.php");
 ?>
