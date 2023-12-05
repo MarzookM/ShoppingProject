f46d0f8938b58f0cb526d1fc79bef1fae0944f10
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-05 05:20:37 -0600

    Update cart.php

--- cart.php
+++ cart.php
@@ -7,22 +7,22 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if(insertProduct($_POST['cNumber']){
-        echo '<div class="alert alert-success" role="alert">Quantity Added.</div>';
+      if(insertProduct($_POST['cNumber'])){
+        echo '<div class="alert alert-success" role="alert">Product Added.</div>';
       }else{
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
     case "Edit":
-      if(updateProduct($_POST['cDesc']){
-        echo '<div class="alert alert-success" role="alert">Quantity edited.</div>';
+      if(updateProduct($_POST['cDesc'])){
+        echo '<div class="alert alert-success" role="alert">Product edited.</div>';
       }else{
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
     case "Delete":
      if(deleteProduct($_POST['cid'])){
-        echo '<div class="alert alert-success" role="alert">Quantity Deleted.</div>';
+        echo '<div class="alert alert-success" role="alert">Course Deleted.</div>';
       }else{
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
@@ -30,7 +30,6 @@ if (isset($_POST['actionType'])) {
     
   }
 }
-
 $CartProduct = selectCartProducts();
 include "view-cart.php";
 include "view-footer.php";
