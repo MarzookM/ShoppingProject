97063c3601d3f6a56a608406542de6506094f8a6
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-05 03:10:18 -0600

    Create model-cart.php

---
+++ model-cart.php
@@ -0,0 +1,18 @@
+<?php
+include("util-db.php");
+
+// Function to fetch cart items for a specific product type
+function getCartItems($productType)
+{
+    $query = "SELECT * FROM Store JOIN $productType ON Store.${productType}ID = ${productType}.${productType}ID 
+              WHERE ${productType}.${productType}Cart = 'y' AND ${productType}.${productType}Quantity > 0";
+    
+    $result = mysqli_query(get_db_connection(), $query);
+
+    if (!$result) {
+        die("Query failed: " . mysqli_error(get_db_connection()));
+    }
+
+    return $result;
+}
+?>
