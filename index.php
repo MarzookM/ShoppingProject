64a1c8c96d3bd70bc4cd0c6db9f60873fc0a7145
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-05 07:44:04 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,35 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?> 
-<object data="WelcomeVid.mp4" width="400" height="300"></object>
+<style>
+        body {
+            margin: 0;
+            overflow: hidden;
+        }
+
+        video {
+            width: 100vw;
+            height: 100vh;
+            object-fit: cover;
+        }
+    </style>
+</head>
+<body>
+    <!-- Embedding the video element -->
+    <video id="welcomeVideo" controls autoplay loop>
+        <source src="WelcomeVid.mp4" type="video/mp4">
+       
+    </video>
+
+    <!-- Include your JavaScript code -->
+    <script>
+        document.getElementById('welcomeVideo').addEventListener('ended', function () {
+            this.currentTime = 0;
+            this.play();
+        });
+
+        document.getElementById('welcomeVideo').play();
+    </script>
 <?php
 include "view-footer.php";
 ?> 
