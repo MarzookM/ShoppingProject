5c972e5233d2e6576289025f80037e0856e12480
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-05 05:35:56 -0600

    Update model-cart.php

--- model-cart.php
+++ model-cart.php
@@ -23,44 +23,4 @@ function selectCartProducts(){
         throw $e;
     }
 }
-
-function insertProduct($cNumber){
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("INSERT INTO `Product` (`ProductQuantity`) VALUES (?)"); 
-        $stmt->bind_param("ss", $cNumber);
-        $success = $stmt->execute();
-        $conn->close();
-        return  $success;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
-function updateProduct($cNumber){
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("update `Product` set `ProductQuantity` = ?" ); 
-        $stmt->bind_param("ssi", $cNumber);
-        $success = $stmt->execute();
-        $conn->close();
-        return $success;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
-function deleteProduct($cid){
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("delete from Product where ProductQuantity =?"); 
-        $stmt->bind_param("i", $cid);
-        $success = $stmt->execute();
-        $conn->close();
-        return $success;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
 ?>
