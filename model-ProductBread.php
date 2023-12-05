4b509a80e2051111cd4504838e5912d4e2cc49c6
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-04 18:29:46 -0600

    Create model-ProductBread.php

---
+++ model-ProductBread.php
@@ -0,0 +1,17 @@
+<?php
+function selectProductBread(){
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT `ProductID`, `ProductName`, `ProductPrice`, `ProductQuantity`, `ProductCart`, `ProductImage` FROM `Product`  "); 
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+?>
