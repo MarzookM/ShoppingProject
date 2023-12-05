a9e72d3d2b613194bfb22eef627157675dbed90b
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-05 05:17:56 -0600

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,32 +1,38 @@
 <?php
 require_once("util-db.php");
 require_once("model-cart.php");
-$pageTitle = "Cart";
+$pageTitle = "Customer";
 include "view-header.php";
 
 if (isset($_POST['actionType'])) {
-    switch ($_POST['actionType']) {
-        case "Add":
-            // Add your logic for adding to cart here
-            break;
-        case "Edit":
-            if (updateCartProductQuantity($_POST['quantity'], $_POST['productId'])) {
-                echo '<div class="alert alert-success" role="alert">Product quantity updated.</div>';
-            } else {
-                echo '<div class="alert alert-danger" role="alert">Error updating product quantity.</div>';
-            }
-            break;
-        case "Delete":
-            if (deleteCartProduct($_POST['productId'])) {
-                echo '<div class="alert alert-success" role="alert">Product removed from cart.</div>';
-            } else {
-                echo '<div class="alert alert-danger" role="alert">Error removing product from cart.</div>';
-            }
-            break;
-    }
+  switch ($_POST['actionType']) {
+    case "Add":
+      if(insertProduct($_POST['cNumber']){
+        echo '<div class="alert alert-success" role="alert">Quantity Added.</div>';
+      }else{
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Edit":
+      if(updateProduct($_POST['cDesc']){
+        echo '<div class="alert alert-success" role="alert">Quantity edited.</div>';
+      }else{
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Delete":
+     if(deleteProduct($_POST['cid'])){
+        echo '<div class="alert alert-success" role="alert">Quantity Deleted.</div>';
+      }else{
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    
+  }
 }
 
-$cartProducts = getCartProducts();
-include "view-cart.php"; // Assuming this is where you display the cart
+$CartProduct = selectCartProducts();
+include "view-cart.php";
 include "view-footer.php";
 ?>
+
