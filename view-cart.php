62f57c7b4e35ea45b1feec6c5a61aaa17b03d779
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-05 21:37:21 -0600

    Update view-cart.php

--- view-cart.php
+++ view-cart.php
@@ -1,9 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Cart</title>
+    <!-- Bootstrap CSS link, adjust as needed -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <style>
+        body {
+            background-color: #333; /* Replace with your preferred dark grey color code */
+            color: white; /* Set text color to white for better visibility */
+        }
+    </style>
+</head>
+<body>
+
 <div class="container mt-5">
     <h1 class="mb-4">Your Cart</h1>
 
     <div class="table-responsive">
-        <table class="table table-bordered">
-            <thead class="table-dark">
+        <table class="table table-bordered table-dark">
+            <thead>
                 <tr>
                     <th scope="col">Name</th>
                     <th scope="col">Quantity</th>
@@ -38,3 +55,11 @@
         <a href="AddCustomer.php" class="btn btn-primary btn-lg">Go to Checkout</a>
     </div>
 </div>
+
+<!-- Bootstrap JS and jQuery scripts, adjust as needed -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+</body>
+</html>
