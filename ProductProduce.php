88f43fb894f72a0fb0c90564977185eb547136b4
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-05 22:37:50 -0600

    Delete ProductProduce.php

--- ProductProduce.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("util-db.php");
-require_once("model-ProductProduce.php");
-$pageTitle = "Drinks";
-include "view-header.php";
-$ProductProduce = selectProductProduce();
-include "view-ProductProduce.php";
-include "view-footer.php";
-?>
