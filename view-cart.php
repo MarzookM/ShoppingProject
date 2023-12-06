898e91de2fc34df2962fe9c4e51119cb425e2686
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-05 21:33:29 -0600

    Update view-cart.php

--- view-cart.php
+++ view-cart.php
@@ -1,37 +1,40 @@
-<h1>Cart Products</h1>
-<div class="table-responsive">
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Quantity</th>
-                <th>Price</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            $totalPrice = 0; // Initialize total price
-            while ($row = $CartProduct->fetch_assoc()) {
-                $finalPrice = $row['ProductPrice'] * $row['ProductQuantity'];
-                $totalPrice += $finalPrice; // Accumulate total price
-            ?>
-                <tr>
-                    <td><?php echo $row['ProductName']; ?></td>
-                    <td><?php echo $row['ProductQuantity']; ?></td>
-                    <td><?php echo $finalPrice; ?></td>
-                </tr>
-            <?php
-            }
-            ?>
-            <tr>
-                <td colspan="2" style="text-align: right;"><strong>Total Price:</strong></td>
-                <td><strong><?php echo $totalPrice; ?></strong></td>
-            </tr>
-        </tbody>
-    </table>
-</div>
+<div class="container mt-5">
+    <h1 class="mb-4">Your Cart</h1>
 
-<!-- Centered "Go to Check Out" button -->
-<div class="d-flex justify-content-center mt-4">
-    <a href="AddCustomer.php" class="btn btn-primary">Go to Check Out</a>
+    <div class="table-responsive">
+        <table class="table table-bordered">
+            <thead class="table-dark">
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Quantity</th>
+                    <th scope="col">Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $totalPrice = 0; // Initialize total price
+                while ($row = $CartProduct->fetch_assoc()) {
+                    $finalPrice = $row['ProductPrice'] * $row['ProductQuantity'];
+                    $totalPrice += $finalPrice; // Accumulate total price
+                ?>
+                    <tr>
+                        <td><?php echo $row['ProductName']; ?></td>
+                        <td><?php echo $row['ProductQuantity']; ?></td>
+                        <td><?php echo $finalPrice; ?></td>
+                    </tr>
+                <?php
+                }
+                ?>
+                <tr>
+                    <td colspan="2" style="text-align: right;"><strong>Total Price:</strong></td>
+                    <td><strong><?php echo $totalPrice; ?></strong></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <!-- Centered "Go to Checkout" button -->
+    <div class="text-center mt-4">
+        <a href="AddCustomer.php" class="btn btn-primary btn-lg">Go to Checkout</a>
+    </div>
 </div>
