dd304877f8216b708a0edea23e8ef721b9eefe98
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-05 23:02:35 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -1,87 +1,72 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?=$pageTitle?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
-        body {
-            margin: 0;
-            padding: 0;
-        }
-
         .big-nav-bar {
             background-color: darkblue;
             color: yellow;
             text-align: center;
-            margin-left: 55px;
-            padding: 30px 15px;
-            width: 100%;
+            padding: 30px 0;
         }
-
         .small-nav-bar {
             background-color: grey;
-            margin: 0;
-            padding: 0;
+            padding: 25px 0;
             text-align: center;
         }
-
-        .big-nav-bar span {
-            font-size: 50px;
-            font-weight: bold;
-            margin-right: auto;
-            display: inline-block;
-        }
-
         .big-nav-bar a,
         .small-nav-bar a {
             color: white;
             text-decoration: none;
             margin-right: 20px;
             font-size: 18px;
-            transition: all 0.3s ease;
-        }
-
-        .big-nav-bar a.cart-icon {
-            float: right;
+            transition: all 0.3s ease; /* Added hover animation */
         }
 
         .big-nav-bar a:hover,
         .small-nav-bar a:hover {
-            transform: scale(1.1);
+
+            transform: scale(1.1); /* Added hover animation */
         }
 
+        /* Adjusting size and hover animation for Cart in big nav bar */
         .big-nav-bar a[href="cart.php"] {
             font-size: 24px;
         }
 
+        /* Centering and adding hover animation for icons in small nav bar */
         .small-nav-bar a {
-            display: inline-block;
-            margin: 0 10px;
+            display: inline-block; /* Display icons next to each other */
+            margin: 0 10px; /* Added margin to create space between icons */
         }
 
         .small-nav-bar a:hover {
-            transform: scale(1.1);
+            transform: scale(1.1); /* Added hover animation */
         }
     </style>
 </head>
 <body>
 
+<!-- Big Nav Bar -->
 <div class="big-nav-bar">
-    <span>MIS-MART</span>
-    <a href="cart.php" class="cart-icon">Cart</a>
+    <span style="font-size: 50px; font-weight: bold; left-margin: 15px;">MIS-MART</span>
+    <a href="cart.php" style="float: right;">Cart</a>
 </div>
 
+<!-- Small Nav Bar -->
 <div class="small-nav-bar">
     <a href="index.php">Home</a>
     <a href="Product.php">Product</a>
     <a href="about.php">About Us</a>
 </div>
 
+<!-- Rest of your content goes here -->
+
+<!-- Your existing navigation code -->
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <!-- Your navigation content goes here -->
+    <!-- ... Your existing code ... -->
 </nav>
+
+<!-- Bootstrap JS and other scripts go here -->
 
 </body>
 </html>
