5f39d2dbff85f504088b47393058396403e56501
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-05 20:09:57 -0600

    Update order.php

--- order.php
+++ order.php
@@ -1,6 +1,13 @@
 <?php
 include("util-db.php");
 
+// Function to wipe all data from the Customer table
+function deleteAllCustomers($conn)
+{
+    $deleteQuery = "TRUNCATE TABLE Customer";
+    mysqli_query($conn, $deleteQuery);
+}
+
 // Function to update the quantity and cart status of all products
 function updateAllProducts($conn)
 {
@@ -10,12 +17,15 @@ function updateAllProducts($conn)
 
 // Check if the request method is POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Wipe all data from the Customer table
+    deleteAllCustomers(get_db_connection());
+
     // Update all products
     updateAllProducts(get_db_connection());
 
     // Add more logic if needed
 
-    echo "Order placed successfully.";
+    echo "All customer data wiped and order placed successfully.";
 } else {
     // Invalid request
     echo "Invalid request.";
