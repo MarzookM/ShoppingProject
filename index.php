aacc5e38f96860186092a9decba91498d59e7af0
MarzookM <98559077+MarzookM@users.noreply.github.com>
2023-12-05 22:39:58 -0600

    Update index.php

--- index.php
+++ index.php
@@ -17,12 +17,12 @@ include "view-header.php";
 </style>
 </head>
 <body>
-    <!-- Embedding the video element -->
+ 
     <video id="welcomeVideo" controls autoplay loop>
         <source src="WelcomeVid.mp4" type="video/mp4">
     </video>
 
-    <!-- Include your JavaScript code -->
+ 
     <script>
         document.getElementById('welcomeVideo').addEventListener('ended', function () {
             this.currentTime = 0;
